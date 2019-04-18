dd899f7a1349b334bfdc61ed9a7ebf4e72645063
Slim <justusburdick@amdg.rockhursths.edu>
2019-04-18 12:00:34 -0500

    rocket progress #2

--- Game-of-Circles---Python-master/GameOfCircles/Rocket.py
+++ Game-of-Circles---Python-master/GameOfCircles/Rocket.py
@@ -5,19 +5,51 @@ class Rocket(Sprite):
     
     mark = 0
     wait = 1000
-    seconds =[1, 2, 3, 4]
+    waitTotal = 4000
     diameterX = 75
     diameterY = 25
     c = color(255, 0, 125)
     w = 2
-    xSpeed = random(0, 4)
+    xSpeed = random(0, 2)
     ySpeed = random(-4, 4)
     
     def move(self):
         
-        for x in self.seconds:
-            if millis() - self.mark < self.wait:
-                self.x += self.xSpeed
-                self.y += -self.ySpeed
-                self.mark += millis()
+        self.x += self.xSpeed
+        self.y += self.ySpeed
+        if self.y < 0 or self.y > height:
+            self.ySpeed *= -1
+        if self.x > width:
+            self.xSpeed *= -1
+        if self.control() == True:
+            self.bounce()
+        else:
+            self.explode()
             
+    def handleCollision(self):
+        if self.control() == True:
+            self.explode
+        else:
+            pass
+        
+    
+    def control(self):
+         
+        if millis() < self.waitTotal:
+            return True
+        else:
+            return False
+       
+    def bounce(self):
+        
+        if millis() - self.mark > self.wait:
+            self.ySpeed = -self.ySpeed
+            self.mark = millis()
+    
+    def explode(self):
+        
+        self.diameterX = 25
+        self.diameterY = 25
+        self.xSpeed = 0
+        self.ySpeed = 0
+        self.c = color(255, 165, 0)
